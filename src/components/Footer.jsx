2f81caa51235850bb388d0142fa56bf41af9db64
varse412 <79421795+varse412@users.noreply.github.com>
2022-07-18 15:10:43 +0530

    Completed app

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-const date=new Date();
-const year=date.getFullYear();
-function Footer(){
-   return <footer>
-   <p >Copyright @ {year}
-   </p>
-   </footer>;
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;
